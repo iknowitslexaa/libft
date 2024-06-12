81915dbee60ea8dec1bb2faba9c17eaecebbdbda
iknowitslexaa <167018550+iknowitslexaa@users.noreply.github.com>
2024-06-12 16:02:14 +0100

    Update ft_putchar_fd.c

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <unistd.h>
 
 void	ft_putchar_fd(char c, int fd)
