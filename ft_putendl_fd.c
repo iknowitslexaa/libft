a9352fb57d110b382ad349d7ce09d29ee52dbf89
iknowitslexaa <167018550+iknowitslexaa@users.noreply.github.com>
2024-06-12 16:02:43 +0100

    Update ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <unistd.h>
 
 void	ft_putendl_fd(char *s, int fd)
